28190bfa5cc6ae825e5b74b713d6c63dbd27f5cc
mdshahanwaz7 <hasmatikhatoon77@gmail.com>
2026-01-10 17:43:32 +0530

    updated

--- Backend/src/middlewares/authmiddleware.js
+++ Backend/src/middlewares/authmiddleware.js
@@ -3,7 +3,7 @@ import jwt from "jsonwebtoken";
 const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
   console.log(authHeader)
-  
+  //md
 
   if (!authHeader || !authHeader.startsWith("Bearer ")) {
     return res.status(401).json({ message: "No token provided" });
